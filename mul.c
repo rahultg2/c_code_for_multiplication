7bedd98b10e53f21a21fde0e356d7d9c3b035a9c
mayanksoni052 <35027941+mayanksoni052@users.noreply.github.com>
2020-10-01 23:24:42 +0530

    Improved code by added comment

--- mul.c
+++ mul.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
 	int a, b;
-	printf("Enter two numbers: ");
+	printf("Enter two numbers: "); // Take input from user
 	scanf("%d %d", &a, &b);
-	printf("%d", a*b);
+	printf("%d", a*b); // Perform Multiplication
 	return 0;
 }
